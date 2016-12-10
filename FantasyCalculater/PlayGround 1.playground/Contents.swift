3f6d7525c905dd281da49c04edb19ec53cb1297a
Student <student@macbook-pro-0404-spencer-chapple.local>
2016-12-10 14:59:38 -0500

    Here I have coded everything in order to calculate all the scores. Now I just have to wright code in order to do a report on each section of your team. As well as simplify the code in order to make it easier on anyone who wants to read it.

--- FantasyCalculater/PlayGround 1.playground/Contents.swift
+++ FantasyCalculater/PlayGround 1.playground/Contents.swift
@@ -5,13 +5,15 @@ import UIKit
 
 //Creating values for all of the different scoring
 
-var points = 0
-let goal = 4
+let defGoal = 6
+let midGoal = 5
+let STGoal = 4
 let assist = 3
 let cleanSheet = 4
+let midCleanSheet = 1
 let bonus1 = 3
 let bonus2 = 2
-let bunus3 = 1
+let bonus3 = 1
 let yellowCard = -1
 let redCard = -3
 let penMiss = -2
@@ -20,6 +22,7 @@ let ownGoal = -2
 var saves = 0
 var minutes = 0
 var goalsConceded = 0
+
 
 //Calculating minutes played
 func played(minutes: Int) -> Int {
@@ -34,7 +37,7 @@ func played(minutes: Int) -> Int {
 
 }
 // Do a calculation based on this player's minutes
-minutes += played(minutes: 45)
+minutes += played(minutes: 1)
 
 // Calculating value for number of saves
 func saves(fromGoals: Int) -> Int {
@@ -43,7 +46,7 @@ func saves(fromGoals: Int) -> Int {
 }
 
 // Do a calculation based on GoalKeepers saves
-saves += saves(fromGoals: 5)
+saves += saves(fromGoals: 1)
 
 //Calculating how many points to subtract when goals are conceded.
 func goalsConceded (goalsAgainst: Int) -> Int {
@@ -53,13 +56,56 @@ func goalsConceded (goalsAgainst: Int) -> Int {
 //Testing goals conceded
 goalsConceded += goalsConceded(goalsAgainst: 4)
 
-//Combining a players score
+// Calculating scores for your GoalKeeper
 
-let totalScore = assist * 1 + redCard + minutes + goalsConceded
+let goalieScore = defGoal * 0 + assist * 0 + yellowCard * 0 + redCard * 0 + bonus1 * 0 + bonus2 * 0 + bonus3 * 0 + cleanSheet * 0 + penMiss * 0 + ownGoal * 0 + played(minutes: 91) + goalsConceded(goalsAgainst: 5)
 
 
-        
-let coutinhoScore = "Coutinho scored \(totalScore + bonus1) points in fantasy this week"
+//Calculating total scores for your defenders
+
+let defendersScore = defGoal * 3 + assist * 2 + yellowCard * 0 + redCard * 0 + bonus1 * 0 + bonus2 * 0 + bonus3 * 0 + cleanSheet * 0 + penMiss * 0 + ownGoal * 0
+
+let defenderOne = played(minutes: 92) + goalsConceded(goalsAgainst: 0)
+
+let defenderTwo = played(minutes: 93) + goalsConceded(goalsAgainst: 0)
+
+let defenderThree = played(minutes: 90) + goalsConceded(goalsAgainst: 2)
+
+let defenderFour = played(minutes: 0) + goalsConceded(goalsAgainst: 0)
+
+let defenderFive = played(minutes: 27) + goalsConceded(goalsAgainst: 0)
+
+let totalDefendersScore = defendersScore + defenderOne + defenderThree + defenderFour + defenderFive
+
+//Calculating scores for your midfielders
+
+let midfieldersScore = midGoal * 2 + assist * 0 + yellowCard * 1 + redCard * 0 + bonus1 * 0 + bonus2 * 0 + bonus3 * 0 + midCleanSheet * 0 + penMiss * 0 + ownGoal * 0
+
+let midfielderOne = played(minutes: 90)
+
+let midfielderTwo = played(minutes: 91)
+
+let midfielderThree = played(minutes: 90)
+
+let midfielderFour = played(minutes: 58)
+
+let midfielderFive = played(minutes: 0)
+
+let totalMidfieldersScore = midfieldersScore + midfielderOne + midfielderTwo + midfielderThree + midfielderFour + midfielderFive
+
+//Calculating your forwards scores
+
+let forwardsScores = STGoal * 3 + assist * 2 + yellowCard * 1 + redCard * 2 + bonus1 * 0 + bonus2 * 0 + bonus3 * 0 + penMiss * 0 + ownGoal * 0
+
+let forwardOne = played(minutes: 75)
+
+let forwardTwo = played(minutes: 88)
+
+let forwardThree = played(minutes: 90)
+
+let totalForwardsScores = forwardsScores + forwardOne + forwardThree
+
+let overallWeeekScore = goalieScore + totalDefendersScore + totalMidfieldersScore + totalForwardsScores
 
 //writing a report on players score
 
@@ -77,4 +123,4 @@ func reportingScore (totalScore: Int) -> String {
     }
 }
 
-reportingScore(totalScore: totalScore)
+//reportingScore(totalScore: totalScore)
